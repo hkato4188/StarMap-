5cf484e0d57d8a7d7338cd1e5efa9b38d1f10f77
Hiroki Kato <hirokikato1@gmail.com>
2024-08-31 16:34:29 -0400

    generated basic spring boot structure

---
+++ src/test/java/dev/hkato/nasa/Nasa/API/CRUD/App.java
@@ -0,0 +1,13 @@
+package dev.hkato.nasa.Nasa.API.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
